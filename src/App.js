89e01df4b6a8562ce23509bbc3d9a4cb5ef99909
DixonOrtiz <dixon.ortizc@gmail.com>
2020-02-11 18:16:14 -0300

    project cleaning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo</h1>
+      <h2>
+        Soy un proyecto de aprendizaje del uso de la librería de la plantilla
+        material-kit-react.
+      </h2>
     </div>
   );
 }
